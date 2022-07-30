8d0c1dc6aad387172f93b5b2c822ce0e27b64e0e
leerauljin <leerauljin@pusan.ac.kr>
2022-07-30 14:33:03 +0900

    FIx1

--- horizon-blink.js
+++ horizon-blink.js
@@ -1,4 +1,26 @@
-"background-color": "#1c1e26",
-"foreground-color": "#bababa",
-"cursor-color":     "#bababa",
-"color-palette-overrides": ["#131519","#e95478","#14d386","#fab795","#30aad7","#b877db","#1fdad9","#c7c7c7","#686868","#e06783","#0af29d","#fac39a","#56c2ea","#c38ce1","#3ce8e6","#ffffff"]
+black       = '#16161c';
+red         = '#e95678'; // red
+green       = '#29d398'; // green
+yellow      = '#fab795'; // yellow
+blue        = '#26bbd9'; // blue
+magenta     = '#ee64ac'; // pink
+cyan        = '#59e1e3'; // cyan
+white       = '#d5d8da'; // light gray
+lightBlack  = '#5b5858'; // medium gray
+lightRed    = '#ec6a88'; // red
+lightGreen  = '#3fdaa4'; // green
+lightYellow = '#fbc3a7'; // yellow
+lightBlue   = '#3fc4de'; // blue
+lightMagenta= '#f075b5'; // pink
+lightCyan   = '#6be4e6'; // cyan
+lightWhite  = '#d5d8da'; // white
+
+t.prefs_.set('color-palette-overrides',
+                    [ black , red     , green  , yellow,
+                    blue     , magenta , cyan   , white,
+                    lightBlack   , lightRed  , lightGreen , lightYellow,
+                    lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', '#bababa');
+t.prefs_.set('foreground-color', '#e0e0e0');
+t.prefs_.set('background-color', '#1c1e26');
